a5b89f2cfb1416088dadc061f822cda5140cb5c3
sol <sol@sito.org>
2022-08-27 13:21:26 -0600

    oops i started over

---
+++ gen.py
@@ -0,0 +1,12 @@
+lvl = input('enter level choice (A for 1-3, B for 4-6, C for 7-10):')
+
+# FIXME this is kinda cruel, should let user type again
+if lvl != 'A' and lvl != 'B' and lvl != 'C':
+    print('you failed!')
+    exit()
+
+# this tells them they did it right
+print(f'you chose {lvl}, winner')
+
+
+# put rest of stuff here

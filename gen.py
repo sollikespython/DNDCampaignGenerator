ec994d857dd1dafa4ed07bedf628e819adb02b22
sol <sol@sito.org>
2022-08-30 14:12:35 -0600

    kind of some functions to start with

--- gen.py
+++ gen.py
@@ -1,3 +1,12 @@
+
+def generate_start_place(num_players, level, alignment):
+    # TODO make this do something cool
+    return f'place = {num_players} + {level} + {alignment} FIXME'
+
+def generate_goal_place(num_players, level, alignment):
+    return 0
+
+
 lvl = input('enter level choice (A for 1-3, B for 4-6, C for 7-10):')
 
 # FIXME this is kinda cruel, should let user type again
@@ -5,5 +14,12 @@ if lvl != 'A' and lvl != 'B' and lvl != 'C':
     print('you failed!')
     exit()
 
-# this tells them they did it right
-print(f'you chose {lvl}, winner')
+num_players = input('number of players (1-10):')
+# FIXME validate 1-10
+alignment = input('alignment:')
+# FIXME validate alignment
+
+start_place = generate_start_place(num_players, lvl, alignment)
+print(start_place)
+
+

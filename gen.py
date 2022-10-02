09419f8c3ff2c5ddeac5f68b376479803eb8eac3
ShaggyBadger <josh.ballard1983@gmail.com>
2022-10-02 02:05:15 -0400

    Fix level selection and fix number of characters

--- gen.py
+++ gen.py
@@ -1,16 +1,21 @@
 
+from formatter import NullFormatter
 import random
+from readline import set_completion_display_matches_hook
 
 def randolist(list):
     rnd = random.randint(0, len(list)-1)
     return list[rnd]
 
-def generate_start_place(num_players, level):
+def generate_start_place(num_players, lvl):
     # TODO make this do something cool
-    if lvl == 'A':
+    if lvl == 'a':
        location_start.pop(1)
-    elif lvl == 'B':
-        #some other pop
+    elif lvl == 'b':
+        # some other pop
+        pass
+    elif lvl == 'c':
+        # some other pop
         pass
     return randolist(location_start)
 
@@ -51,18 +56,49 @@ location_goal = [
     'Inside a very large plant, or maybe a normal sized plant and you are very small.'
 ]
 
-lvl = input('enter level choice (A for 1-3, B for 4-6, C for 7-10):')
+def lvl_select():
+    # returns a string of a, b, or c
+    selection_valid = False
+    user_selection = None
 
-# FIXME this is kinda cruel, should let user type again
-if lvl != 'A' and lvl != 'B' and lvl != 'C':
-    print('you failed!')
-    exit()
+    while selection_valid is False:
+        valid_selections = ('a', 'b', 'c')
+        selection_input = input('Enter level choice (A for 1-3, B for 4-6, C for 7-10: ').lower()
 
+        if selection_input not in valid_selections:
+            input('That  is not a valid selection, press any key to try again... ')
+            
+        else:
+            user_selection = selection_input
+            selection_valid = True
 
-num_players = input('number of players (1-10):')
-# FIXME validate 1-10
+    return user_selection
 
-start_place = generate_start_place(num_players, lvl)
+def num_players():
+    # validate 1 - 10
+    # No idea if this is a good way to make a check like this
+    selection_valid = False
+    user_selection = None
+
+    while selection_valid is False:
+        valid_selections = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        selection_input = input('Enter the number of players (1 - 10): ')
+
+        try:
+            selection_input = int(selection_input)
+
+            if selection_input not in valid_selections:
+                input('That is not a valid selection, press any key to try again... ')
+
+            else:
+                user_selection = selection_input
+                selection_valid = True
+        except:
+            input('Please enter an integer between 1 and 10. Press any key to try again... ')
+
+    return user_selection
+
+start_place = generate_start_place(num_players(), lvl_select())
 print(start_place)
 
 
